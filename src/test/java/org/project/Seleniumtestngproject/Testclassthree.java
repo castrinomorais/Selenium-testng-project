5579368451b68ea9a8f2608c155b32e38f161a81
castrinomorais <castrino.s@outlook.com>
2025-03-27 11:29:42 +0530

    First Commit

---
+++ src/test/java/org/project/Seleniumtestngproject/Testclassthree.java
@@ -0,0 +1,128 @@
+package org.project.Seleniumtestngproject;
+
+import java.io.File;
+import java.time.Duration;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import org.testng.annotations.Test;
+
+public class Testclassthree {
+
+	@Test
+	public void textBox() {
+		WebDriver driver;
+		driver = new ChromeDriver();
+		driver.get("https://www.leafground.com/input.xhtml");
+		// Type your Name
+		PageFactory.initElements(driver, PageObjects.class);
+		PageObjects.typeYourName.sendKeys("Castrino");
+
+		// Append Country to this City.
+		PageObjects.appendCountryToThisCity.sendKeys("India");
+
+		// Verify if text box is disabled
+		boolean enabledStatus = PageObjects.txtbox.isEnabled();
+		System.out.println(enabledStatus);
+
+		// Clear the typed text
+		PageObjects.clearText.clear();
+
+		// Retrieve the typed text
+		String textWithinTheBox = PageObjects.retrieveText.getAttribute("value");
+		System.out.println(textWithinTheBox);
+		driver.close();
+	}
+
+	@Test
+	public void uploadOperation() {
+		WebDriver driver;
+		driver = new ChromeDriver();
+		// To automate process of upload by sending the filepath of the file to be uploaded in the sendkeys
+		driver.get("https://www.leafground.com/file.xhtml;jsessionid=node010nx464jtc8vzxsmrg4fp0yu9138.node0");
+		PageFactory.initElements(driver, PageObjects.class);
+		PageObjects.uploadButton.sendKeys("C:\\Users\\DELL\\Downloads\\timetableexams.pdf");
+		driver.close();
+	}
+
+	@Test
+	public void downloadAndValidation() {
+		WebDriver driver;
+		driver = new ChromeDriver();
+		driver.get("https://www.leafground.com/file.xhtml;jsessionid=node010nx464jtc8vzxsmrg4fp0yu9138.node0");
+        //To automate download file and check if the file is downloaded in the repository or not 		
+		PageFactory.initElements(driver, PageObjects.class);
+		PageObjects.downloadButton.click();
+		File fileLocation = new File("C:\\Users\\DELL\\Downloads");
+		File[] totalFiles = fileLocation.listFiles();
+		for (File f : totalFiles) {
+			if (f.getName().equals("Testleaf Logo")) {
+				System.out.println("file has Downloaded");
+				break;
+			}
+
+		}
+		driver.close();
+	}
+
+	@Test
+	public void explicitWait() {
+		// Initialize WebDriver
+		WebDriver driver = new ChromeDriver();
+
+		try {
+			// Navigate to the specified URL
+			driver.get("https://www.leafground.com/waits.xhtml");
+
+			// Initialize WebDriverWait with a timeout of 10 seconds
+			WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+			// Wait until the element is visible
+			WebElement element = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("j_idt87:j_idt90")));
+
+			// Find the element and click it
+			PageFactory.initElements(driver, PageObjects.class);
+			PageObjects.Iamhere.click();
+//         
+		} catch (Exception e) {
+			// Handle any exceptions that occur during execution
+			System.out.println("An error occured");
+		} finally {
+			// Close the driver
+			driver.close();
+		}
+	}
+
+	@Test
+	public void hyperLinks() {
+
+		WebDriver driver;
+		driver = new ChromeDriver();
+		driver.navigate()
+				.to("https://www.leafground.com/link.xhtml;jsessionid=node010nx464jtc8vzxsmrg4fp0yu9138.node0");
+
+		// Take me to Dashboard
+		PageFactory.initElements(driver, PageObjects.class);
+		PageObjects.gotoDashboard.click();
+		driver.navigate().back();
+
+		// Find the url without clicking
+		PageFactory.initElements(driver, PageObjects.class);
+		String url = PageObjects.findmyDestination.getAttribute("href");
+		System.out.println(url);
+
+		// To count the number of links
+		List<WebElement> totalLinks = driver.findElements(By.tagName("a"));
+		int countOfLinks = totalLinks.size();
+		System.out.println(countOfLinks);
+		driver.close();
+
+	}
+}
---
+++ src/test/java/org/project/Seleniumtestngproject/Testclasstwo.java
@@ -0,0 +1,143 @@
+package org.project.Seleniumtestngproject;
+
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.PageFactory;
+
+
+import org.testng.annotations.Test;
+
+public class Testclasstwo {
+
+	@Test
+	public void windowHandlesExercise() {
+		WebDriver driver;
+		driver = new ChromeDriver();
+		driver.get("https://www.leafground.com/window.xhtml");
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		// Click openbutton a new window opens and we click the browser icon in the  child window.
+		String parentwindow = driver.getWindowHandle();
+		PageFactory.initElements(driver, PageObjects.class);
+		PageObjects.openbutton.click();
+		Set<String> allwindowhandles = driver.getWindowHandles();
+		for (String wh : allwindowhandles) {
+			if (!wh.equals(parentwindow)) {
+				driver.switchTo().window(wh);
+
+			}
+			PageFactory.initElements(driver, PageObjects.class);
+			PageObjects.browser.click();
+			driver.close();
+		}
+	}
+
+	@Test
+	public void windowHandlesExercisetwo() {
+		WebDriver driver;
+		driver = new ChromeDriver();
+		driver.get("https://www.leafground.com/window.xhtml");
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+
+		// parent window contains window handle value of the parent window
+		String parentwindow = driver.getWindowHandle();
+
+		// Clicking the openmultipleButton opens child window
+		PageFactory.initElements(driver, PageObjects.class);
+		PageObjects.openmultipleButton.click();
+
+		// allwindowhandles set contain the window handle values of all the windows opened
+		Set<String> allwindowhandles = driver.getWindowHandles();
+
+		// we use size method over the set to count the number of window handles values of the windows opened 
+        int numberofwindows = allwindowhandles.size();
+
+		// The value returned by the size method stored in a variable is printed and also all windowhandle values
+        //are printed
+	    System.out.println(numberofwindows);
+		System.out.println(allwindowhandles);
+		driver.quit();
+	}
+
+	@Test
+	public void seleniumIframes() {
+		// To click a button inside an iframe and get the text
+		WebDriver driver;
+		driver = new ChromeDriver();
+		driver.get("https://www.leafground.com/frame.xhtml");
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		driver.switchTo().frame(0);
+		PageFactory.initElements(driver, PageObjects.class);
+		PageObjects.clickMebuttonTwo.click();
+		String textvalue = PageObjects.clickMebuttonTwo.getText();
+		System.out.println(textvalue);
+		driver.quit();
+	}
+
+	@Test
+	public void numberOfFramesInWebpage() {
+		// To find the number of iframes
+		WebDriver driver;
+		driver = new ChromeDriver();
+		driver.get("https://www.leafground.com/frame.xhtml");
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		List<WebElement> frames = driver.findElements(By.tagName("iframe"));
+		int totalframes = frames.size();
+		System.out.println("The total number of frames in webpage " + +totalframes);
+		driver.close();
+	}
+
+	@Test
+	public void checkBox() {
+		WebDriver driver;
+		driver = new ChromeDriver();
+		driver.get("https://www.leafground.com/radio.xhtml");
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		driver.get("https://www.leafground.com/checkbox.xhtml");
+
+		// Select a basic checkbox
+		PageFactory.initElements(driver, PageObjects.class);
+		PageObjects.basicCheckbox.click();
+
+		// Selecting multiple checkboxes
+		PageObjects.javaCheckbox.click();
+		PageObjects.pythonCheckbox.click();
+
+		// To Select a toggle switch slider
+		PageObjects.toogleSwitch.click();
+
+		// To verify if the check box is disabled
+		boolean enabledStatusOfCheckbox = PageObjects.disabledCheckbox.isEnabled();
+		System.out.println(enabledStatusOfCheckbox);
+		driver.close();
+	}
+
+	@Test
+	public void seleniumRadioButtons() {
+		WebDriver driver;
+		driver = new ChromeDriver();
+		driver.get("https://www.leafground.com/radio.xhtml");
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+
+		// Selecting a radio button
+		PageFactory.initElements(driver, PageObjects.class);
+		PageObjects.fireFoxRadioButton.click();
+
+		// Status of the radio button safari 
+		PageFactory.initElements(driver, PageObjects.class);
+		boolean selectionStatusOfSafari = PageObjects.Safari.isSelected();
+		boolean displayStatusOfSafari = PageObjects.Safari.isDisplayed();
+		boolean enabledStatusOfSafari = PageObjects.Safari.isEnabled();
+
+		System.out.println(selectionStatusOfSafari);
+		System.out.println(displayStatusOfSafari);
+		System.out.println(enabledStatusOfSafari);
+		driver.close();
+	}
+
+}
